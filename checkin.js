8336ab7a37835dc46b094ab62fea884f762c5028
Zoybzo <61582821+Zoybzo@users.noreply.github.com>
2022-03-13 16:14:46 +0800

    Initial commit

---
+++ checkin.js
@@ -0,0 +1,64 @@
+const axios = require('axios');
+
+const checkIn = async (cookie) => {
+    return axios({
+        method: 'post',
+        url: 'https://ikuuu.co/user/checkin',
+        headers: {
+            'Cookie': cookie
+        },
+        data: {
+            token: "ikuuu_network"
+        }
+    });
+};
+
+const checkInAndGetStatus = async (cookie) => {
+    const checkInMessage = (await checkIn(cookie))?.data?.message;
+
+    return {
+        '签到情况': checkInMessage
+    };
+};
+
+const pushplus = (token, infos) => {
+    const titleCheckInMessage = infos?.[0]['签到情况'];
+
+    const title = (
+        '签到情况: ' + `${titleCheckInMessage}`
+    ).slice(0, 100);
+
+    const data = {
+        token,
+        title,
+        content: JSON.stringify(infos),
+        template: 'json'
+    };
+    console.log(data);
+
+    return axios({
+        method: 'post',
+        url: `http://www.pushplus.plus/send`,
+        data
+    });
+};
+
+const ikuuuCheckIn = async () => {
+    try {
+        const cookies = process.env.COOKIES?.split('&&') ?? [];
+
+        const infos = await Promise.all(cookies.map(async cookie => await checkInAndGetStatus(cookie)));
+        console.log(infos);
+
+        const PUSHPLUS = process.env.PUSHPLUS;
+
+        if (PUSHPLUS && infos.length) {
+            const pushResult = (await pushplus(PUSHPLUS, infos))?.data?.msg;
+            console.log(pushResult);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+ikuuuCheckIn();
